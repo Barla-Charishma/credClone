6508ba9aca39a9603898d32e69e1beccead6d24a
mundrupardhasaradhi <Pardhumundru1423@gmail.com>
2024-03-13 13:03:41 +0530

    code giving error

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex absolute-center footer'>
-      By Charishma Barla
+      By Mundru Pardha saradhi
     </div>
   )
 }
